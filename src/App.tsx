4801ae4b9a8a1acb73e28fab30b2f60dc5b62807
Camila Calistro Frigéri <149757865+Camilafrigeri@users.noreply.github.com>
2024-08-20 18:51:07 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,124 @@ import './App.css';
 
 function App() {
   return (
-    
+    <h1>LANDING VAI VIR AQ</h1>
+    <br /> <br />
+    <!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="index.css">
+    <title>Início</title>
+  </head>
+  <body>
+    <nav id="navbar">
+    <ul class="navbar-items flexbox-col">
+      <li class="navbar-logo flexbox-left">
+        <a class="navbar-item-inner flexbox">
+          <svg style="color: rgb(220, 138, 221);" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"></rect><line x1="128" y1="192" x2="128" y2="224" fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="12"></line><circle cx="84" cy="140" r="10"></circle><circle cx="172" cy="140" r="10"></circle><line x1="128" y1="48" x2="128" y2="88" fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="12"></line><polyline points="144 176 128 192 112 176" fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="12"></polyline><line x1="96" y1="53" x2="96" y2="88" fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="12"></line><line x1="160" y1="53" x2="160" y2="88" fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="12"></line><path d="M32,136V51.3a8,8,0,0,1,13.7-5.6L67.6,67.6h0A100.8,100.8,0,0,1,128,48a100.8,100.8,0,0,1,60.4,19.6h0l21.9-21.9A8,8,0,0,1,224,51.3V136c0,48.6-43,88-96,88S32,184.6,32,136Z" fill="#dc8add" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="12"></path></svg>
+        </a>
+      </li>
+      <li class="navbar-item flexbox-left">
+        <a href="https://camilafrigeri.web.app/sobre.html" class="navbar-item-inner flexbox-left">
+          <div class="navbar-item-inner-icon-wrapper flexbox">
+            <ion-icon name="search-outline"></ion-icon>
+          </div>
+          <span class="link-text">Sobre mim</span>
+        </a>
+      </li>
+      <li class="navbar-item flexbox-left">
+        <a href="https://camilafrigeri.web.app/" class="navbar-item-inner flexbox-left">
+          <div class="navbar-item-inner-icon-wrapper flexbox">
+            <ion-icon name="home-outline"></ion-icon>
+          </div>
+          <span class="link-text">Início</span>
+        </a>
+      </li>
+      <li class="navbar-item flexbox-left">
+        <a href="https://camilafrigeri.web.app/projetos.html" class="navbar-item-inner flexbox-left">
+          <div class="navbar-item-inner-icon-wrapper flexbox">
+            <ion-icon name="folder-open-outline"></ion-icon>
+          </div>
+          <span class="link-text">Projetos</span>
+        </a>
+      </li>
+
+      <li class="navbar-item flexbox-left">
+        <a href="https://camilafrigeri.web.app/contatos.html" class="navbar-item-inner flexbox-left">
+          <div class="navbar-item-inner-icon-wrapper flexbox">
+            <ion-icon name="people-outline"></ion-icon>
+          </div>
+          <span class="link-text">Contatos</span>
+        </a>
+      </li>
+      <li class="navbar-item flexbox-left">
+        <a href="https://camilafrigeri.web.app/apoie.html" class="navbar-item-inner flexbox-left">
+          <div class="navbar-item-inner-icon-wrapper flexbox">
+            <ion-icon name="chatbubbles-outline"></ion-icon>
+          </div>
+          <span class="link-text">Apoie-me</span>
+        </a>
+      </li>
+      <li class="navbar-item flexbox-left">
+        <a href="https://camilafrigeri.web.app/tutoriais.html" class="navbar-item-inner flexbox-left">
+          <div class="navbar-item-inner-icon-wrapper flexbox">
+            <ion-icon name="settings-outline"></ion-icon>
+          </div>
+          <span class="link-text">Blog</span>
+        </a>
+      </li>
+    </ul>
+  </nav>
+    <div class="header">
+      <div class="inner-header flex">
+      <h1>Bem vindos!</h1>
+      </div>
+      <div>
+      <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+      viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+      <defs>
+      <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+      </defs>
+      <g class="parallax">
+      <use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(255,182,193,0.7" />
+      <use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(255,192,203,0.5)" />
+      <use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(240,128,128,0.3)" />
+      <use xlink:href="#gentle-wave" x="48" y="7" fill="rgb(221,160,221)" />
+      </g>
+      </svg>
+      </div>
+      </div>
+        <br />
+        <br />
+        <h3>Acesse o menu no canto esquerdo!</h3>
+        <br />
+        <br />
+        <p style="color: black;">Este site é dedicado a ser uma junção de minhas informações, redes sociais, história e etc, para facilitar o acesso.</p>
+        <br />
+        <p style="color: black;">Obrigada pelo interesse, me contate em caso de dúvidas!</p>
+        <br />
+        <p style="color: black;">Até mais, com amor,</p>
+        <br />
+        <p style="color: pink; background-color: black;">Camila</p>
+        <br />
+        <br />
+        <div class="heart">
+          <div class="heartbeat">❤️</div>
+          <div class="heartecho">❤️</div>
+        </div>
+        <div class="footer-clean">
+        <footer>
+                     
+                        <p class="copyright">Camila Calistro Frigéri  </p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+      </div>
+      </body>
+</html>
+
   );
 }
 
